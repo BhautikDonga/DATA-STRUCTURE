703dfdaf478b01a3674cee8c77d78e07c2717bf4
BhautikDonga <34915439+BhautikDonga@users.noreply.github.com>
2018-10-29 18:32:04 +0530

    Add files via upload

---
+++ Singly_LinkedList_deletion.c
@@ -0,0 +1,149 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+	int  value;
+	struct node *link;
+};
+
+struct node* insertAtFront(struct node* head,int val){
+	struct node* newNode;
+	newNode = (struct node*)malloc(sizeof(struct node));
+	if(newNode == NULL){
+        printf("new block is not creating\n");
+        return head;
+	}
+
+	newNode->value = val;
+    newNode->link = head; //here we don't check whether head is NULL or Not.
+    head = newNode;
+
+	return head;
+}
+
+struct node* deleteFront(struct node* head){
+    struct node* nodeToBeDeleted;
+    nodeToBeDeleted = head;
+
+    if(head->link == NULL){
+        free(head);
+        printf("List is empty\n");
+        return;
+    }
+    head = head->link;
+    free(nodeToBeDeleted);
+    return head;
+
+}
+
+void deleteEnd(struct node** head){
+    struct node* temp = *head;
+
+    if(temp->link == NULL){
+        free(temp);
+        *head = NULL;
+        printf("Now your List is empty\n");
+        return;
+    }
+    while(temp->link->link != NULL)
+        temp = temp->link;
+
+    free(temp->link);
+    temp->link = NULL;
+}
+
+struct node* deleteSpecific(struct node* head,int val){
+    struct node* nodeToBeDeleted,*temp = head;
+
+    if(head==NULL){
+        printf("List is empty\n");
+        return head;
+    }
+
+    if(head->link == NULL && head->value == val){
+        //condition for only node
+        //in other case we find out our value from sitting at previous Node ,here there is only single Node
+        nodeToBeDeleted = head;
+        head = head->link;
+        free(nodeToBeDeleted);
+        return head;
+    }
+
+    while((temp->link != NULL) && (temp->link->value != val) )
+        temp = temp->link;
+
+    if(temp->link == NULL){
+        printf("Entered value is not found\n");
+    }
+    else{
+    nodeToBeDeleted = temp->link;
+    temp->link = temp->link->link;
+    free(nodeToBeDeleted);
+    }
+
+    return head;
+};
+
+void display(struct node *head){
+    struct node *temp=head;
+
+    if(temp==NULL){
+        printf("Your list is empty\n");
+        return;
+    }
+
+    while(temp != NULL){
+        printf("%d ",temp->value);
+        temp = temp->link;
+    }
+}
+
+
+
+void main(){
+
+	struct node * head;
+	head= NULL;
+
+	int n,val;
+
+	printf("Enter 1 for insert\n");
+	printf("Enter 2 for delete from front \n");
+	printf("Enter 3 for delete from end\n");
+    printf("Enter 4 for delete specific value \n");
+	printf("Enter 5 for display\n");
+	printf("Enter 6 for Exit \n");
+	scanf("%d",&n);
+
+	while(n!=6){
+	switch(n){
+	case 1:
+		printf("Enter the value  you want:\n");
+		scanf("%d",&val);
+		head = insertAtFront(head,val);
+		break;
+	case 2:
+		head = deleteFront(head);
+		break;
+
+	case 3:
+	    deleteEnd(&head); //pass address , for if there is only node to be deleted use
+		break;
+
+    case 4:
+        printf("Enter the value:\n");
+        scanf("%d",&val);
+        head = deleteSpecific(head,val);
+        break;
+	case 5:
+		display(head);
+		break;
+	default:
+		printf("Enter value between 1 to 6:\n");
+
+	}
+	printf("Enter which type of operation you want to do again\n");
+	scanf("%d",&n);
+    }
+}
+
---
+++ Singly_Linkedlist_insertion.c
@@ -0,0 +1,157 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+	int  value;
+	struct node *link;
+	};
+
+struct node* insertAtFront(struct node* head,int val){
+	struct node* newNode;
+	newNode = (struct node*)malloc(sizeof(struct node));
+	if(newNode == NULL){
+        printf("new block is not creating\n");
+        return head;
+	}
+
+	newNode->value = val;
+    newNode->link = head; //here we don't check whether head is NULL or Not.
+    head = newNode;
+
+	return head;
+}
+
+void insertAtSpecific(struct node * head,int pval,int val){
+    struct node* newNode,*temp = head;
+	newNode = (struct node*)malloc(sizeof(struct node));
+	if(newNode == NULL){
+		printf("new block is not creating\n");
+        return;
+	}
+
+	newNode->value = val;
+
+	while(temp->value != pval)
+            temp = temp->link;
+
+    newNode->link = temp->link;
+    temp->link = newNode;
+
+}
+
+void insertAtEnd(struct node** head,int val){
+    struct node *newNode,*temp;
+    temp = (*head);
+
+	newNode = (struct node*)malloc(sizeof(struct node));
+	if(newNode == NULL){
+		printf("new block is not creating\n");
+        return;
+	}
+
+	newNode->value = val;
+
+	if((*head) == NULL){
+        newNode->link = NULL;
+        (*head) = newNode;
+        return;             // this line is  most important. check it by removing it.
+	}
+
+	while(temp->link != NULL)
+        temp= temp->link;
+
+    temp->link = newNode;
+    newNode->link = NULL;
+
+}
+
+void insert_Order(struct node** head,int val){
+    struct node* newNode,*temp = *head;
+	newNode = (struct node*)malloc(sizeof(struct node));
+	if(newNode == NULL)
+		printf("new block is not creating\n");
+
+	newNode->value = val;
+
+
+	if(*head == NULL || val <= (*head)->value){
+            newNode->link = *head;
+            *head = newNode;
+	}
+	else{
+        while(temp->link != NULL && temp->link->value <val){
+            temp= temp->link;
+        }
+
+        newNode->link = temp->link;
+        temp->link = newNode;
+
+	}
+}
+
+void display(struct node *head){
+    struct node *temp=head;
+
+    if(temp==NULL){
+        printf("Your list is empty\n");
+        return;
+    }
+
+    while(temp != NULL){
+        printf("%d ",temp->value);
+        temp = temp->link;
+    }
+}
+
+void main(){
+
+	struct node * head;
+	head= NULL;
+
+	int n,val,pval;
+
+	printf("Enter 1 for insert at front:\n");
+	printf("Enter 2 for insert at end \n");
+	printf("Enter 3 for insert at specific value :\n");
+    printf("Enter 4 for insert in order \n");
+	printf("Enter 5 for display\n");
+	printf("Enter 6 for Exit \n");
+	scanf("%d",&n);
+
+	while(n!=6){
+	switch(n){
+	case 1:
+		printf("Enter the value  you want:\n");
+		scanf("%d",&val);
+		head = insertAtFront(head,val);
+		break;
+	case 2:
+	    printf("Enter the value :\n");
+		scanf("%d",&val);
+		insertAtEnd(&head,val);
+		break;
+
+	case 3:
+	    printf("Enter after which value you want to be entered new value:\n");
+		scanf("%d",&pval);
+		printf("Enter the value you want to be inserted:\n");
+		scanf("%d",&val);
+		insertAtSpecific(head,pval,val);
+		break;
+
+    case 4:
+        printf("Enter the value:\n");
+        scanf("%d",&val);
+        insert_Order(&head,val);
+        break;
+	case 5:
+		display(head);
+		break;
+	default:
+		printf("Enter value between 1 to 6:\n");
+
+	}
+	printf("Enter which type of operation you want to do again\n");
+	scanf("%d",&n);
+    }
+}
