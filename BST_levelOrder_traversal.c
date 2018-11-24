b359a10dcecc25555b6c58826b77647448280c17
Bhautik Donga <34915439+BhautikDonga@users.noreply.github.com>
2018-11-24 14:17:53 +0530

    Add files via upload

---
+++ BST_levelOrder_traversal.c
@@ -0,0 +1,129 @@
+//for binary tree, traversal by level order method.
+//Level order traversal of a tree is breadth first traversal for the tree.
+#include<stdio.h>
+#include<stdlib.h>
+
+struct treeNode{
+    int value;
+    struct treeNode *lptr;
+    struct treeNode *rptr;
+};
+
+struct queueNode{
+    struct treeNode * data;
+    struct queueNode * ptr;
+};
+struct queueNode * front= NULL;
+struct queueNode * rear = NULL;
+
+void insert_queue(struct treeNode* value){
+    struct queueNode* newNode;
+    newNode = (struct queueNode*) malloc(sizeof(struct queueNode));
+    if(newNode == NULL){
+        printf("Malloc is failed\n");
+        return;
+    }
+
+    newNode->data = value;
+    newNode->ptr = NULL;
+    if(front == NULL)
+        front = rear = newNode;
+    else{
+        rear->ptr = newNode;
+        rear = newNode;
+    }
+}
+
+void dequeue(){
+    struct queueNode * temp;
+    //we dont have to put front = null condition because dequeue is nt called when front = null.
+    temp = front->ptr;
+    free(front);
+    front = temp;
+}
+
+void levelOrder_traversal(struct treeNode* r){
+    if(r == NULL){
+        printf("Tree is Empty\n");
+        return;
+    }
+
+    insert_queue(r);
+
+    while(front != NULL){
+        printf("%d ",front->data->value);
+
+        if(front->data->lptr != NULL) insert_queue(front->data->lptr);
+        if(front->data->rptr != NULL) insert_queue(front->data->rptr);
+
+        dequeue();
+    }
+
+}
+
+struct treeNode* insert_val(struct treeNode * root,int val){
+    struct treeNode * newNode,*temp;
+    temp = root;
+    newNode = (struct treeNode *)malloc(sizeof(struct treeNode));
+    if(newNode==NULL){
+        printf("Error in malloc\n");
+        return root;
+    }
+
+    newNode->value = val;
+    newNode->lptr = NULL;
+    newNode->rptr = NULL;
+
+    if(root == NULL){
+        root = newNode;
+        return root;
+    }
+
+    while(1){
+        if(val < temp->value){
+            if(temp->lptr == NULL){
+                temp->lptr = newNode;
+                break;
+            }
+            temp = temp->lptr;
+        }
+        else{
+            if(temp->rptr == NULL){
+                temp->rptr = newNode;
+                break;
+            }
+            temp = temp->rptr;
+        }
+    }
+    return root;
+}
+
+
+void main(){
+    int n,val;
+    struct treeNode *root;
+    root = NULL;
+
+    printf("Enter 1 for insert value\n");
+    printf("Enter 2 for level order traversal\n");
+    printf("Enter 3 for EXIT\n");
+    scanf("%d",&n);
+
+    while(n!=3){
+    switch(n){
+    case 1:
+        printf("Enter the value you want to insert\n");
+        scanf("%d",&val);
+        root = insert_val(root,val);
+        break;
+    case 2:
+        levelOrder_traversal(root);
+        break;
+    default:
+        printf("Enter perfect value:\n");
+    }
+    printf("Enter which type of operation you want to do again\n");
+    scanf("%d",&n);
+    }
+
+}
---
+++ MaxHeap_tree.c
@@ -0,0 +1,81 @@
+//for binary tree with n nodes we can say that leaf node is startig from floor(n/2)+1 to n.
+//In this we take value and put it at its proper place and fullfill heap properties.
+#include<stdio.h>
+#define N 20
+
+void insert_val(int a[],int i){
+    if(i==0){
+        return;
+    }
+    int temp;
+
+    temp = a[i];
+    while( i>0 && (temp > a[(i-1)/2]) ){
+        a[i] = a[(i-1)/2];
+        i=(i-1)/2;
+    }
+    a[i] = temp;
+}
+
+void delete_max(int a[],int n){
+    int temp,parent,child;
+    if(n==0){
+        printf("Heap is empty\n");
+        return;
+    }
+    //item = a[0]; this is for if we want to return deleted value.
+    a[0] = a[n-1];
+    n--;
+    temp = a[0];
+    parent = 0; child = 1; //child equal to 1 because there is 2 child for every parent so default LC = 1 and RC = 2;
+    while( child < n){
+        if(child + 1 < n && a[child] < a[child+1] )
+            child ++;
+        if(temp > a[child]) break;
+        a[parent] = a[child];
+        parent = child;
+        child = 2*parent +1;
+    }
+    a[parent] = temp;
+}
+
+void display(int a[],int n){
+    if(n==0) printf("The Heap is empty\n");
+    int i;
+    for(i=0;i<n;i++)
+        printf("%d ",a[i]);
+    printf("\n");
+}
+
+void main(){
+    int heap[N];
+    int n,i=0;
+
+    printf("Enter 1 for insert the element\n");
+    printf("Enter 2 for delete the root element\n");
+    printf("Enter 3 for print the heap tree\n");
+    printf("Enter 4 for EXIT\n");
+    scanf("%d",&n);
+
+    while(n!=4){
+        switch(n){
+            case 1:
+                printf("Enter the value:\n");
+                scanf("%d",&heap[i]);
+                insert_val(heap,i);
+                i++;
+                break;
+            case 2:
+                delete_max(heap,i);
+                i--;
+                break;
+            case 3:
+                display(heap,i);
+                break;
+            default:
+                printf("Enter the value between 1 to 4\n");
+        }
+        printf("\nEnter 1/2/3/4\n");
+        scanf("%d",&n);
+    }
+}
